f472015bdfa9a5083dca967541f2341981b3aee3
albane tonnellier <albanetonnellier@gmail.com>
2021-01-05 17:44:39 +0000

    footer added

---
+++ src/component/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+	<title> Helper.vue </title>
+	</div>
+</template>
+
+<script>
+import formHelper from './components/formHelper.vue'
+
+export default {
+	components: {
+	'form-helper': formHelper
+},
+data() {
+	return {
+
+	}
+},
+methods: {
+	
+}
+</script>
