680ddf39c95d1b901b3da2e58b94316fe4504c01
albane tonnellier <albanetonnellier@gmail.com>
2021-01-06 13:00:44 +0000

    blog content

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,7 @@
     <h2>Add new blog post</h2>
     <form>
       <label>Blog title:</label>
+      <input type="text" v-model.lazy="blog.title">
+      <label>Blog content:</label>
     </form>
 </template>
