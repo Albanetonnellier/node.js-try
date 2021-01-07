0a3d5343cb6da596691799bf8c8ac6b09bdeb351
albane tonnellier <albanetonnellier@gmail.com>
2021-01-07 15:42:03 +0000

    template added for show.blog

--- src/component/showBlog.vue
+++ src/component/showBlog.vue
@@ -1,23 +1,6 @@
 <template>
-	<div>
-		<show-blogs></show-blogs>
+	<div id="show-blogs">
+		<h1>All blog Articles</h1>
+		<div-v-for="" class="single-blog"></div>
 	</div>
 </template>
-
-<script>
-import addBlog from './components/addBlog.vue';
-import showBlogs from './components/showBlogs.vue';
-
-export default {
-	components: {
-	'add-blog': addBlog.
-	'show-blogs': showBlogs
-	},
-	data() {
-	return {
-
-	}
-	},
-}
-
-</script>
