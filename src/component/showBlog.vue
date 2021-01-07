e7bbeea494c6a1948c924e81e737776dbf42552d
albane tonnellier <albanetonnellier@gmail.com>
2021-01-07 15:38:46 +0000

    script added

--- src/component/showBlog.vue
+++ src/component/showBlog.vue
@@ -3,3 +3,20 @@
 		<add-blog></add-blog>
 	</div>
 </template>
+
+<script>
+import addBlog from './components/addBlog.vue';
+import addBlog from './components/addBlog.vue';
+
+export default {
+	components: {
+	'add-blog': addBlog
+	},
+	data() {
+	return {
+
+	}
+	},
+}
+
+</script>
