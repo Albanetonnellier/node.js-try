6987f8834535da04b2ce1c5a7ccc982c9745a8eb
albane tonnellier <albanetonnellier@gmail.com>
2021-01-09 15:14:17 +0000

    import components

--- src/component/routes.js
+++ src/component/routes.js
@@ -1,4 +1,7 @@
+import showBlogs from '.components/showBlogs.vue';
+import addBlog from './components/addBlog.vue';
+
 export default [
 	{ path: '/', component:},
-	{},
+	{ path: '/add', component: },
 ]
